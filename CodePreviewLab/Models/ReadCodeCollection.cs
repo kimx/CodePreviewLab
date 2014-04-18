68b9e2d982994a53ea3e78c5b13f22e83e048676
kimx <kimxinfo@gmail.com>
2014-04-18 15:39:54 +0800

    1

---
+++ CodePreviewLab/Models/ReadCodeCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodePreviewLab.Models
+{
+    public class ReadCodeCollection : List<ReadCodeModel>
+    {
+        public ReadCodeCollection Add(string filePath, string descript = null)
+        {
+            ReadCodeModel m = new ReadCodeModel(filePath, descript);
+            this.Add(m);
+            return this;
+        }
+
+    }
+}
---
+++ CodePreviewLab/Models/ReadCodeModel.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodePreviewLab.Models
+{
+    public class ReadCodeModel
+    {
+
+        public ReadCodeModel(string filePath, string descript = null)
+        {
+            this.FilePath = filePath;
+            this.Descript = descript;
+        }
+
+        public string FilePath { get; set; }
+
+        public string FileName { get; set; }
+
+        public string Descript { get; set; }
+
+        public string Content { get; set; }
+
+
+
+    }
+}
